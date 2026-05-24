95ed8774579b447ae41b93246c07931e764dfe3c
Adarsh Anand <adarshanand1816@gmail.com>
2026-05-24 17:27:02 +0530

    initial clean commit

---
+++ test.py
@@ -0,0 +1,6 @@
+import datasets, transformers, huggingface_hub, tokenizers
+
+print(datasets.__version__)
+print(transformers.__version__)
+print(huggingface_hub.__version__)
+print(tokenizers.__version__)
